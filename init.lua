0c69eb75b2ab1432a1b4162bc07be9a08dd2b730
trobjohn <152633511+trobjohn@users.noreply.github.com>
2025-12-20 02:33:02 -0500

    Update init

--- init.lua
+++ init.lua
@@ -1,10 +1,5 @@
 require("config")
 
--- repl setup
-local repl = require("repl")
-vim.keymap.set("v", "<leader>r", repl.send_visual_to_right_term, {silent=true} )
-
-
 -- lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -19,3 +14,30 @@ vim.opt.rtp:prepend(lazypath)
 
 -- plugins
 require("plugins")
+
+-- repl code
+local repl = require("repl")
+
+-- Openers (normal mode)
+--vim.keymap.set("n", "<leader>p", repl.open_python, { desc = "Open Python REPL" })
+--vim.keymap.set("n", "<leader>i", repl.open_ipython, { desc = "Open IPython REPL" })
+--vim.keymap.set("n", "<leader>r", repl.open_r, { desc = "Open R REPL" })
+
+-- Runners (visual mode)
+vim.keymap.set("v", "<leader>p", repl.run_visual_python, { desc = "Run selection in Python REPL" })
+vim.keymap.set("v", "<leader>i", repl.run_visual_ipython, { desc = "Run selection in IPython" })
+vim.keymap.set("v", "<leader>r", repl.run_visual_r, { desc = "Run selection in R" })
+
+-- Cell runners (normal mode)
+vim.keymap.set("n", "<leader>p", repl.run_cell_python, { desc = "Run cell in Python REPL" })
+vim.keymap.set("n", "<leader>i", repl.run_cell_ipython, { desc = "Run cell in IPython" })
+vim.keymap.set("n", "<leader>r", repl.run_cell_r, { desc = "Run cell in R" })
+
+-- Line numbers
+vim.opt.number = true        -- absolute line numbers
+vim.opt.relativenumber = false
+
+vim.keymap.set("n", "<leader>ln", function()
+  vim.opt.number = not vim.opt.number:get()
+end, { desc = "Toggle line numbers" })
+
