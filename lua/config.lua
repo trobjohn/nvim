62f2c5fd8664e13441979eb600977c2d82fbe4d0
trobjohn <152633511+trobjohn@users.noreply.github.com>
2026-01-14 07:43:04 -0500

    nav edit

--- lua/config.lua
+++ lua/config.lua
@@ -20,13 +20,12 @@ end, { noremap = true, silent = true })
 
 -- Ctrl+s to save:
 vim.keymap.set("i", "<C-s>", "<Esc>:w<CR>a", { silent = true })
-vim.keymap.set({ "n", "v" }, "<C-s>", ":w<CR>", { silent = true })
+
 
 
 -- Ctrl+q/+w/+e to exit/save-and-exit/hard exit:
 vim.keymap.set("i", "<C-q>", "<Esc>:q<CR>",{silent=true})
 vim.keymap.set({"n", "v"}, "<C-q>", ":q<CR>", {silent=true})
-
 --vim.keymap.set("i", "<C-w>", "<Esc>:wq<CR>", {silent=true})
 --vim.keymap.set({"n","v"}, "<C-w>", ":wq<CR>", {silent=true})
 
@@ -34,7 +33,7 @@ vim.keymap.set("i", "<C-e>", "<Esc>:qa<CR>", {silent=true})
 vim.keymap.set({"n","v"}, "<C-e>", ":qa<CR>", {silent=true})
 
 
--- Timeout delay
+-- Timeout
 vim.opt.timeout = true
 vim.opt.timeoutlen = 2500   -- mappings: be patient
 vim.opt.ttimeout = true
@@ -63,11 +62,43 @@ end, { desc = "Toggle whitespace" })
 
 -- home and end motions
 -- ...normal mode
-vim.keymap.set("n", "<C-h>", "^", { silent = true })
-vim.keymap.set("n", "<C-l>", "$", { silent = true })
+vim.keymap.set("n", "<C-f>", "^", { silent = true })
+vim.keymap.set("n", "<C-g>", "$", { silent = true })
 -- ...insert mode
-vim.keymap.set("i", "<C-h>", "<C-o>^", { silent = true })
-vim.keymap.set("i", "<C-l>", "<C-o>$", { silent = true })
+vim.keymap.set("i", "<C-f>", "<C-o>^", { silent = true })
+vim.keymap.set("i", "<C-g>", "<C-o>$", { silent = true })
+
+-- Window navigation
+vim.keymap.set({"i","n"}, "<C-h>", "<C-w>h", { silent = true })
+vim.keymap.set({"i","n"}, "<C-j>", "<C-w>j", { silent = true })
+vim.keymap.set({"i","n"}, "<C-k>", "<C-w>k", { silent = true })
+vim.keymap.set({"i","n"}, "<C-l>", "<C-w>l", { silent = true })
+
+-- Ctrl selection
+-- Visual extend left/right from insert or normal
+vim.keymap.set({ "n", "i" }, "<C-S-h>", "<Esc>vh", { silent = true })
+vim.keymap.set({ "n", "i" }, "<C-S-l>", "<Esc>vl", { silent = true })
+vim.keymap.set({"i","n"}, "<C-f>", "<C-w>h", { silent = true })
+vim.keymap.set({"i","n"}, "<C-g>", "<C-w>l", { silent = true })
+
+
+
+-- Neo-tree automatically
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.argc() == 1 and vim.fn.isdirectory(vim.fn.argv(0)) == 1 then
+      vim.cmd("Neotree")
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  callback = function()
+    if vim.bo.filetype == "neo-tree" and vim.fn.winnr("$") == 1 then
+      vim.cmd("quit")
+    end
+  end,
+})
 
 
 -- Visual-mode: send selection
